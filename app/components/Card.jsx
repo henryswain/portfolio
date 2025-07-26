f7b81133960470633730ef87de94b8228389249d
henryswain <henryswain3141@gmail.com>
2025-07-25 20:23:41 -0500

    updated Card component
    - added hover effect to make it easier for users to know the card is clickable

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
-export default function Card({aspectRatio, imagePath, linkPath, xsSize, smSize, mdSize, lgSize, textContent}) {
+export default function Card({aspectRatio = "4/3", imagePath, linkPath, xsSize, smSize, mdSize, lgSize, textContent, textColor = "white", textContainerHeight = "h-9"} = {}) {
     return (
         <Link href={linkPath}>
-            <img src={imagePath} alt="clickable image for computer science category" className={`aspect-${aspectRatio} object-cover rounded-t-lg`} />
-            <div className='relative h-1/8 w-full'>
-                <h1 className={`text-nowrap absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white ${xsSize} sm:text-${smSize} md:text-${mdSize} lg:text-${lgSize}`}>{textContent}</h1>
+            <div className='bg-black'>
+                <img src={imagePath} alt="clickable image for computer science category" className={`aspect-${aspectRatio} object-cover rounded-t-lg hover:opacity-75 transition-opacity duration-250`} />
+            </div>
+            <div className={`relative ${textContainerHeight} w-full`}>
+                <h1 className={`text-nowrap absolute mt-1 left-1/2 -translate-x-1/2 text-${textColor} text-${xsSize} sm:text-${smSize} md:text-${mdSize} lg:text-${lgSize}`}>{textContent}</h1>
             </div>
         </Link>
     )
