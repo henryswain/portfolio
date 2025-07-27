35b362d60864580840484b5f3d3c000a0711df38
henryswain <henryswain3141@gmail.com>
2025-07-27 14:21:41 -0500

    fixed bug with card images images not being rounded

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Card({aspectRatio = "4/3", imagePath, linkPath, xsSize, smSize, mdSize, lgSize, textContent, textColor = "white", textContainerHeight = "h-9"} = {}) {
     return (
         <Link href={linkPath}>
-            <div className='bg-black'>
+            <div className='bg-black rounded-t-lg'>
                 <img src={imagePath} alt="clickable image for computer science category" className={`aspect-${aspectRatio} object-cover rounded-t-lg hover:opacity-75 transition-opacity duration-250`} />
             </div>
             <div className={`relative ${textContainerHeight} w-full`}>
