6be1ed33604dda85f171a5cc5263d9693807535a
henryswain <henryswain3141@gmail.com>
2025-07-29 18:35:52 -0500

    ComputerScienceContent update
    - ajusted spacing to improve visual appeal
    - adjusted font size props in cards to account for the lack of space
    based on the adjusted aspect ratios

--- app/components/ComputerScienceContent.jsx
+++ app/components/ComputerScienceContent.jsx
@@ -74,8 +74,8 @@ export default function page() {
     return (
         <>
             <div className="relative">
-                <div className="bg-teal-950 left-1/2 top-8 -translate-x-1/2 absolute h-147 sm:h-220 md:h-125 w-70 sm:w-110 md:w-190 lg:w-240 lg:h-158 rounded-lg grid grid-cols-12 md:grid-cols-18">
-                    <div className="aspect-16/10 col-span-12 md:col-span-9 md:col-start-1 m-4 bg-white rounded-lg">
+                <div className="bg-teal-950 left-1/2 top-8 -translate-x-1/2 absolute h-145 sm:h-220 md:h-125 w-70 sm:w-110 md:w-190 lg:w-240 lg:h-155 rounded-lg grid grid-cols-12 md:grid-cols-18">
+                    <div className="aspect-16/10 col-span-12 md:col-span-9 md:col-start-1 mx-4 mt-4 bg-white rounded-lg">
                         <Card 
                             imagePath="/career-quest-screenshots/career-quest-home.png"
                             linkPath="/Portfolio/computer-science/CareerQuest"
@@ -83,28 +83,28 @@ export default function page() {
                             textColor="black"
                             xsSize="2xl"
                             smSize="2xl"
-                            mdSize="3xl"
-                            lg="4xl"
+                            mdSize="2xl"
+                            lgSize="2xl"
                             textContent="CareerQuest"
                         />
                     </div>
 
 
-                    <div className="aspect-16/10 col-span-12 md:col-start-10 md:col-span-9 m-4 bg-white rounded-lg">
+                    <div className="aspect-16/10 col-span-12 md:col-start-10 md:col-span-9 mx-4 md:mt-4 bg-white rounded-lg">
                         <Card 
-                            imagePath="/film-track-screenshots/film-track-all-movies.png"
+                            imagePath="/film-track-screenshots/film-track-watched.png"
                             linkPath="/Portfolio/computer-science/FilmTrack"
                             aspectRatio="16/9"
                             textColor="black"
                             xsSize="2xl"
                             smSize="2xl"
-                            mdSize="3xl"
-                            lg="4xl"
+                            mdSize="2xl"
+                            lgSize="2xl"
                             textContent="Film Track"
                         />
                     </div>
 
-                    <div className="aspect-16/10 col-span-12 md:col-span-9 md:col-start-1 m-4 bg-white rounded-lg">
+                    <div className="aspect-16/10 col-span-12 md:col-span-9 md:col-start-6 md:mr-13 mx-4 bg-white rounded-lg">
                         <Card 
                             imagePath="/honors-contract-screenshots/honors-contract-sleep.png"
                             linkPath="/Portfolio/computer-science/HonorsContract"
@@ -112,8 +112,8 @@ export default function page() {
                             textColor="black"
                             xsSize="2xl"
                             smSize="2xl"
-                            mdSize="3xl"
-                            lg="4xl"
+                            mdSize="2xl"
+                            lgSize="2xl"
                             textContent="Honors Contract"
                         />
                     </div>
