e62010962c42f04732913aea5653f385d10faac8
henryswain <henryswain3141@gmail.com>
2025-08-09 14:35:49 -0500

    updated layout.js
    - removed head tag
    - added proper font import
    - added meta information for mobile

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,23 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+export const viewport = {
+  viewport: 'width=device-width, initial-scale=1'
+}
+
+const inter = Inter({
+  subsets: ['latin']
+})
+
 
 export default function DashboardLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        {/* added google fonts import */}
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
-      </head>
+    <html lang="en" className={inter.className}>
       <body>
         {/* Layout UI */}
 
         {/* Place children where you want to render a page or nested layout */}
-        <main className="background-gradient">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   )
