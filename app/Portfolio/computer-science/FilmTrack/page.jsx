44a74a27cf5f539a1444b82331523957707ee27a
henryswain <henryswain3141@gmail.com>
2026-06-04 21:10:12 -0500

    changed key features list type from dashed to bulleted in FilmTrack page

--- app/Portfolio/computer-science/FilmTrack/page.jsx
+++ app/Portfolio/computer-science/FilmTrack/page.jsx
@@ -13,17 +13,17 @@ export default function page() {
                      (Marcus Bodnar and Aron Ramiscal) to expand upon Joey Wroble's glorified CRUD 
                      application - Movie-Watchlist.
                 </p>
-                <p className="mb-2">Key Features:</p>
-                <ul className="mb-4 list-outside">
-                    <li>- User authentication with JWT tokens</li>
-                    <li>- Add movies with titles, comments, and ratings</li>
-                    <li>- Mark movies as watched/unwatched</li>
-                    <li>- Write and view movie reviews</li>
-                    <li>- Filter movies by status (all, watched, not watched, my movies)</li>
-                    <li>- Import picture as a background (.jpeg, .jpg, .png, .gif)</li>
-                    <li>- Schedule viewing dates with calendar event export</li>
-                    <li>- Administrative privileges for managing all content</li>
-                    <li>- The app uses Python (FastAPI), VueJS, and MongoDB for the tech stack.</li>
+                <p className="mb-2 text-teal-300"><strong>Key Features:</strong></p>
+                <ul className="mb-4 pl-8 list-disc">
+                    <li>User authentication with JWT tokens</li>
+                    <li>Add movies with titles, comments, and ratings</li>
+                    <li>Mark movies as watched/unwatched</li>
+                    <li>Write and view movie reviews</li>
+                    <li>Filter movies by status (all, watched, not watched, my movies)</li>
+                    <li>Import picture as a background (.jpeg, .jpg, .png, .gif)</li>
+                    <li>Schedule viewing dates with calendar event export</li>
+                    <li>Administrative privileges for managing all content</li>
+                    <li>The app uses Python (FastAPI), VueJS, and MongoDB for the tech stack.</li>
                 </ul>
             </div>
     )
@@ -52,7 +52,7 @@ export default function page() {
     console.log("link: ", link)
     return (
         <>
-            <div className="flex gap-8 flex-col items-center bg-gradient-filmtrack sm:bg-gradient-filmtrack-sm md:bg-gradient-filmtrack-md">
+            <div className="flex gap-8 flex-col items-center bg-gradient pb-8">
                 <NavBar/>
                 <ProjectContent 
                     description={description} 
@@ -64,8 +64,8 @@ export default function page() {
                     date={date} 
                 />
                 <div className="flex w-full flex-row justify-between">
-                    <Link href="/Portfolio/computer-science/CareerQuest" className="pl-15 sm:pr-20 text-lg pb-4">&#10094; Previous Project</Link>
-                    <Link href="/Portfolio/computer-science/HonorsContract" className="pr-15 sm:pr-20 text-lg pb-4">Next Project &#10095;</Link>
+                    <Link href="/Portfolio/computer-science/CareerQuest" className="pl-15 sm:pr-20 text-lg">&#10094; Previous Project</Link>
+                    <Link href="/Portfolio/computer-science/HonorsContract" className="pr-15 sm:pr-20 text-lg">Next Project &#10095;</Link>
                 </div>
             </div>
         </>
